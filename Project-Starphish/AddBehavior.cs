31a8d4755e47804b7a782b5f6f40eb90d10d62ef
Stefanos Loss <stefanos818@gmail.com>
2014-04-06 12:12:07 -0400

    Added a class

---
+++ Project-Starphish/AddBehavior.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace GUI
+{
+    public partial class FormMain
+    {
+        public void mainAddBehaviors()
+        {
+            MessageBox.Show("Sff");
+        }
+    }
+}
